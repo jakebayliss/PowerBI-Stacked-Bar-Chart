e60b6845d692962d4f1aeb873ff9bd6f53f33305
Maksim Galiev <v-makgal@microsoft.com>
2018-09-10 15:24:23 +0300

    added source code

---
+++ src/utils.ts
@@ -0,0 +1,260 @@
+module powerbi.extensibility.visual.visualUtils {
+    import IAxisProperties = powerbi.extensibility.utils.chart.axis.IAxisProperties;
+    import TextMeasurementService = powerbi.extensibility.utils.formatting.textMeasurementService;
+    import TextProperties = powerbi.extensibility.utils.formatting.TextProperties;
+    import IValueFormatter = powerbi.extensibility.utils.formatting.IValueFormatter;
+
+    const DisplayUnitValue: number = 1;
+
+    export function calculateBarCoordianates(data: VisualData, settings: VisualSettings, barHeight: number): void {
+        let isCategoricalAxisType: boolean = settings.categoryAxis.axisType === "categorical";
+
+        data.dataPoints.forEach(point => {
+            let height, width, x, y: number;
+
+            if (data.axes.yIsScalar && !isCategoricalAxisType) {
+                height = barHeight < 0 ? 0 : barHeight;
+            } else {
+                height = data.axes.y.scale.rangeBand();
+            }
+
+            if (point.shiftValue) {
+                x = data.axes.x.scale(point.shiftValue);
+            } else {
+                let xValue = 0;
+                if (data.axes.x.dataDomain[0] > 0) {
+                    xValue = data.axes.x.dataDomain[0];
+                }
+                x = data.axes.x.scale(xValue);
+            }
+
+            let widthValue: number = point.valueForWidth as number;
+            if (data.axes.x.scale(widthValue + point.shiftValue) < 0) {
+                width = 1;
+            } else {
+                width = data.axes.x.scale(widthValue + point.shiftValue) - x;
+            }
+
+            if (data.axes.yIsScalar && !isCategoricalAxisType) {
+                y = data.axes.y.scale(point.category) - barHeight / 2;
+            } else {
+                y = data.axes.y.scale(point.category);
+            }
+
+            point.barCoordinates = {
+                height: height,
+                width: width < 1 && width !== 0 ? 1 : width,
+                x: x,
+                y: y
+            };
+        });
+    }
+
+    export function calculateLabelCoordinates(data: VisualData,
+                                            settings: categoryLabelsSettings,
+                                            metadata: VisualMeasureMetadata,
+                                            chartWidth: number,
+                                            isLegendRendered: boolean) {
+        if (!settings.show) {
+            return;
+        }
+
+        let dataPointsArray: VisualDataPoint[] = data.dataPoints;
+
+        let dataLabelFormatter: IValueFormatter = formattingUtils.createFormatter(settings.displayUnits,
+                                                                        settings.precision,
+                                                                        metadata.cols.value,
+                                                                        formattingUtils.getValueForFormatter(data));
+
+        let textPropertiesForWidth: TextProperties = formattingUtils.getTextProperties(settings);
+        let textPropertiesForHeight: TextProperties = formattingUtils.getTextPropertiesForHeightCalculation(settings);
+
+        dataPointsArray.forEach(dataPoint => {
+            let formattedText: string = dataLabelFormatter.format(dataPoint.value);
+            textPropertiesForHeight.text = formattedText;
+
+            let textWidth: number = TextMeasurementService.measureSvgTextWidth(textPropertiesForWidth, formattedText);
+            let textHeight: number = TextMeasurementService.estimateSvgTextHeight(textPropertiesForHeight);
+
+            let barHeight: number = dataPoint.barCoordinates.height;
+
+            if (settings.overflowText || textHeight +
+                (settings.showBackground ? DataLabelHelper.labelBackgroundHeightPadding : 0) < barHeight) {
+                let dy: number = dataPoint.barCoordinates.y + dataPoint.barCoordinates.height / 2 + (textHeight - 3) / 2,
+                dx: number = DataLabelHelper.calculatePositionShift(settings, textWidth, dataPoint, chartWidth, isLegendRendered);
+
+                if (dx !== null) {
+                    dataPoint.labelCoordinates = {
+                        x: dx,
+                        y: dy,
+                        width: textWidth,
+                        height: textHeight
+                    };
+                } else {
+                    dataPoint.labelCoordinates = null;
+                }
+            } else {
+                dataPoint.labelCoordinates = null;
+            }
+        });
+    }
+
+    export function getNumberOfValues(dataView: DataView): number {
+        const columns: DataViewMetadataColumn[] = dataView.metadata.columns;
+        let valueFieldsCount: number = 0;
+
+        for (let columnName in columns) {
+            const column: DataViewMetadataColumn = columns[columnName];
+
+            if (column.roles && column.roles[Field.Value]) {
+                ++valueFieldsCount;
+            }
+        }
+
+        return valueFieldsCount;
+    }
+
+    export function getLineStyleParam(lineStyle) {
+        let strokeDasharray;
+
+        switch (lineStyle) {
+            case "solid":
+                strokeDasharray = "none";
+                break;
+            case "dashed":
+                strokeDasharray = "7, 5";
+                break;
+            case "dotted":
+                strokeDasharray = "2, 2";
+                break;
+        }
+
+        return strokeDasharray;
+    }
+
+    export function getUnitType(xAxis: IAxisProperties): string {
+        if (xAxis.formatter
+            && xAxis.formatter.displayUnit
+            && xAxis.formatter.displayUnit.value > DisplayUnitValue) {
+
+            return xAxis.formatter.displayUnit.title;
+        }
+
+        return null;
+    }
+
+    export function getTitleWithUnitType(title, axisStyle, axis: IAxisProperties): string {
+        let unitTitle = visualUtils.getUnitType(axis) || "No unit";
+        switch (axisStyle) {
+            case "showUnitOnly": {
+                return unitTitle;
+            }
+            case "showTitleOnly": {
+                return title;
+            }
+            case "showBoth": {
+                return `${title} (${unitTitle})`;
+            }
+        }
+    }
+
+    export const DimmedOpacity: number = 0.4;
+    export const DefaultOpacity: number = 1.0;
+
+    export function getFillOpacity(selected: boolean, highlight: boolean, hasSelection: boolean, hasPartialHighlights: boolean): number {
+        if ((hasPartialHighlights && !highlight) || (hasSelection && !selected)) {
+            return DimmedOpacity;
+        }
+
+        return DefaultOpacity;
+    }
+
+    const CategoryMinHeight: number = 16;
+    const CategoryMaxHeight: number = 130;
+    const CategoryContinuousMinHeight: number = 1;
+
+    export function calculateBarHeight(
+        visualDataPoints: VisualDataPoint[],
+        visualSize: ISize,
+        categories: string[],
+        categoryInnerPadding: number,
+        yScale: any,
+        axisType: string): number {
+
+        let currentBarHeight = visualSize.height / categories.length;
+        let barHeight: number = 0;
+
+        if (axisType === "categorical") {
+            let innerPadding: number = categoryInnerPadding / 100;
+            barHeight = d3.min([CategoryMaxHeight, d3.max([CategoryMinHeight, currentBarHeight])]) * (1 - innerPadding);
+        } else {
+            let dataPoints = [...visualDataPoints];
+
+            let dataPointsCount: number = dataPoints.length;
+
+            if (dataPointsCount < 4) {
+                let devider: number = 3.75;
+                barHeight = visualSize.height / devider;
+            } else {
+                let devider: number = 3.75 + 1.25 * (dataPointsCount - 3); 
+                barHeight = visualSize.height / devider;
+            }
+        }
+
+        return barHeight;
+    }
+
+    export function getLabelsMaxWidth(group: d3.selection.Group): number {
+        const widths: Array<number> = [];
+
+        group.forEach((item: any) => {
+            let dimension: ClientRect = item.getBoundingClientRect();
+            widths.push(d3.max([dimension.width, dimension.height]));
+        });
+
+        if (group.length === 0) {
+            widths.push(0);
+        }
+
+        return d3.max(widths);
+    }
+
+    export function getLabelsMaxHeight(group: d3.selection.Group): number {
+        const heights: Array<number> = [];
+
+        group.forEach((item: any) => {
+            let dimension: ClientRect = item.getBoundingClientRect();
+            heights.push(dimension.height);
+        });
+
+        if (group.length === 0) {
+            heights.push(0);
+        }
+
+        return d3.max(heights);
+    }
+
+    export function GetYAxisTitleHeight(valueSettings: valueAxisSettings): number {
+
+        let textPropertiesForHeight: TextProperties = {
+            fontFamily: valueSettings.titleFontFamily,
+            fontSize: valueSettings.titleFontSize.toString()
+        };
+
+        return TextMeasurementService.estimateSvgTextHeight(textPropertiesForHeight);
+    }
+
+    export function GetXAxisTitleHeight(categorySettings: categoryAxisSettings): number {
+
+        let textPropertiesForHeight: TextProperties = {
+            fontFamily: categorySettings.titleFontFamily,
+            fontSize: categorySettings.titleFontSize.toString()
+        };
+
+        return TextMeasurementService.estimateSvgTextHeight(textPropertiesForHeight);
+    }
+
+    export function isSelected(selected: boolean, highlight: boolean, hasSelection: boolean, hasPartialHighlights: boolean): boolean {
+        return !(hasPartialHighlights && !highlight || hasSelection && !selected);
+    }
+}
